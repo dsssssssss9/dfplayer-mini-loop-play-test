636dacaa36bf4755aabff8675c21f55b5c9c803e
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-03-01 22:55:16 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onButtonPressed(Button.A, function () {
+    dfplayermini.playLoopAllFiles()
+})
+dfplayermini.connect(SerialPin.P16, SerialPin.P2)
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+	
+})
